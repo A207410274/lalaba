f336c975e57d0b5710d4bb2dccdf8c5a53c98206
A207410274 <43174355+A207410274@users.noreply.github.com>
2019-02-19 20:47:41 +0800

    Add files via upload

---
+++ w01/w01-2-Variable-var-let-const/app.js
@@ -0,0 +1,36 @@
+var name ='HuaiWen Liu';
+console.log(name);
+name='劉懷文';
+console.log(name);
+name='2019';
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting='Hello';
+console.log(greeting);
+
+let name2 ='HuaiWen Liu';
+console.log(name2);
+name2='劉懷文';
+console.log(name2);
+name2='2019';
+console.log(name2);
+
+/*const name3 ='HuaiWen Liu';
+console.log(name3);
+name3='劉懷文';
+console.log(name3);
+name3='2019';
+console.log(name3);
+*/
+const person ={
+    name:'LHW',
+    age:18
+};
+person.name ='LiuHuaiWen';
+person.age ='19'
+console.log(person);
+const numbers=[1,2,3,4,5,6];
+numbers.push(7);
+console.log(numbers);
---
+++ w01/w01-3-data-types/app.js
@@ -0,0 +1,29 @@
+//primitive
+
+//string
+const name ='LiuHuaiWen';
+console.log(typeof name);
+//Number
+const age=19;
+console.log(typeof age);
+//Boolean
+const haskids=false;
+console.log(typeof haskids);
+//Null
+const car=null;
+console.log(typeof car);
+//undefined
+let test;
+console.log(typeof text);
+//Symbol
+const sym=Symbol();
+console.log(typeof sym);
+//----------------------------
+
+//Array
+const hobbies =['swim'];
+console.log(typeof hobbies);
+//Objects
+const today =new Date();
+console.log(today);
+console.log(typeof today);
